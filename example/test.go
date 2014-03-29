522e629540b6b5e245b9b5b5bc5235e23e14c88a
qianmin <qianmin@meituan.com>
2014-03-29 14:37:48 +0800

    test.go

--- example/test.go
+++ example/test.go
@@ -25,9 +25,9 @@ func main() {
 	modelname := "./gbdt.model"
 	start := time.Now()
 	trainpath := "./train.data"
-	//trainpath:="/opt/tmp/gbdt/alltrain.data"
+	//trainpath:="/opt/tmp/gbdt/data/train.data"
 	train_sample_number := 4584
-	//train_sample_number:=458334
+	//train_sample_number:=4186052
 	dataset := &gbdt.DataSet{}
 	dataset.LoadDataFromFile(trainpath, train_sample_number)
 	g := gbdt.NewGBDT()
@@ -39,12 +39,11 @@ func main() {
 	}
 	latency := time.Since(start)
 	fmt.Println("train time:", latency)
-
 	start2 := time.Now()
 	testpath := "./test.data"
-	//testpath:="/opt/tmp/gbdt/alltest.data"
-	test_sample_number := 4584
-	//test_sample_number:=458334
+	//testpath:="/opt/tmp/gbdt/data/smalltest.data"
+	//test_sample_number := 837210
+	test_sample_number:=4584
 	testdataset := &gbdt.DataSet{}
 	testdataset.LoadDataFromFile(testpath, test_sample_number)
 
@@ -57,28 +56,34 @@ func main() {
 
 	g.Load(model)
 	samples := testdataset.GetSamples()
-	var T, F float32 = 0, 0
 	tree_count := gbdt.Conf.GetTreecount()
+	var click_sum,no_click_sum float64
 	for i := 0; i < len(samples); i++ {
-		pre := gbdt.LogitCtr(g.Predict(samples[i], tree_count))
-		var label int = 0
-		if pre >= 0.5 {
-			label = 1
-		} else {
-			label = -1
-		}
-		if label == samples[i].GetLabel() {
-			T += 1
-		} else {
-			F += 1
+		if samples[i].GetLabel()==1 {
+			click_sum+=float64(samples[i].GetWeight())
+		}else{
+			no_click_sum+=float64(samples[i].GetWeight())
 		}
 	}
-	fmt.Println("precision rate:", T/float32(test_sample_number))
-
 	latency = time.Since(start2)
 	fmt.Println("precision time:", latency)
+	auc := gbdt.NewAuc()
+	for j := 0; j < len(samples); j++ {
+		p := gbdt.LogitCtr(g.Predict(samples[j], tree_count))
+		auc.Add(float64(p), float64(samples[j].GetWeight()),samples[j].GetLabel())
+	}
+	fmt.Println("auc:", auc.CalculateAuc())
+	auc.PrintConfusionTable()
+	/*FeatureMapFile:="./feature.map"
+	feature_data,err:=ioutil.ReadFile(FeatureMapFile)
+	if err!=nil {
+		log.Fatal(err)
+	}
+	feature_map:=gbdt.LoadFeatureMap(string(feature_data))
 	feature_weight_list:=g.GetFeatureWeight()
 	for i := 0; i < len(feature_weight_list); i++ {
-		fmt.Println(feature_weight_list[i].Key,":",feature_weight_list[i].Value)
-	}
+		fid:=feature_weight_list[i].Key
+		fmt.Println(feature_map[fid],":",feature_weight_list[i].Value)
+	}*/
+		
 }
